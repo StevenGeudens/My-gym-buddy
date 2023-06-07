84e9b313cd28c0916d7514709b96b8c2a9bb81d2
Steven Geudens <Geudens.Steven@hotmail.com>
2023-06-07 14:22:56 +0200

    rm sensitive data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  apiNinjasApiKey: 'yXMQlhfEJwhggKN8t9fgSw==T594iezu7mRZ3dt9',
+  apiNinjasApiKey: '',
   firebaseConfig: {
-    apiKey: 'AIzaSyDY_hhqGQQZjar_7U-p91_tvbkmmUYgOU8',
-    authDomain: 'my-gym-buddy-f6a1d.firebaseapp.com',
-    projectId: 'my-gym-buddy-f6a1d',
-    storageBucket: 'my-gym-buddy-f6a1d.appspot.com',
-    messagingSenderId: '69589360778',
-    appId: '1:69589360778:web:a3b0f679dc413abca28d73'
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
   }
 };
